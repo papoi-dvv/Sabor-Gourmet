40c315feaad2d1dbbaf60235d9057fe997d2cb3a
papoi-dvv <paulo.santos@tecsup.edu.pe>
2025-11-10 11:23:45 -0500

    :v

---
+++ src/main/java/com/saborgourmet/SaborGourmetApplication.java
@@ -0,0 +1,13 @@
+package com.saborgourmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaborGourmetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaborGourmetApplication.class, args);
+	}
+
+}
